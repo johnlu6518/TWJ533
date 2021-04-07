154be7454680b52d7dfff89630f451263811021b
Saber422 <81108166+Saber422@users.noreply.github.com>
2021-04-07 11:35:03 +0800

    Allow GAS #2

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -105,8 +105,9 @@ class CarInterfaceBase():
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
       events.add(EventName.espDisabled)
-    if cs_out.gasPressed:
-      events.add(EventName.gasPressed)
+    #AllowGAS2
+    #if cs_out.gasPressed:
+    #  events.add(EventName.gasPressed)
     if cs_out.stockFcw:
       events.add(EventName.stockFcw)
     if cs_out.stockAeb:
