99acb3d38817b3ce60dc5395f1b5aaf1f60fa997
Comma Device <device@comma.ai>
2021-04-14 16:35:40 +0800

    Resize reverse gear alert size to small

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -691,7 +691,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
     ET.PERMANENT: Alert(
       "Reverse\nGear",
       "",
-      AlertStatus.normal, AlertSize.full,
+      AlertStatus.normal, AlertSize.small,
       Priority.LOWEST, VisualAlert.none, AudibleAlert.none, 0., 0., .2, creation_delay=0.5),
     ET.IMMEDIATE_DISABLE: ImmediateDisableAlert("Reverse Gear"),
     ET.NO_ENTRY: NoEntryAlert("Reverse Gear"),
