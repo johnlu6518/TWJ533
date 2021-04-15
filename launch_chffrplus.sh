e786c553446df5a55acffe9f16e39d4c75bfeb2b
Chad2_Peng <chad2_peng@askey.com.tw>
2021-04-15 15:41:05 +0800

    Revert "Dump log to file"
    This reverts commit 5db18114f60d699cd7f0ea2d1c85a3960fe1faed.

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -221,9 +221,7 @@ function launch {
 
   # start manager
   cd selfdrive/manager
-  #Pon 20210407 Dump log to file
-  #./build.py && ./manager.py
-  ./build.py > /sdcard/log/build_log_$(date +"%Y%m%d_%H%M%S").txt && ./manager.py > /sdcard/log/launch_log_$(date +"%Y%m%d_%H%M%S").txt
+  ./build.py && ./manager.py
 
   # if broken, keep on screen error
   while true; do sleep 1; done
